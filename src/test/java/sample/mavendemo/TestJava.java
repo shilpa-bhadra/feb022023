8e091662a2d3f7b82fbed674a790875f04809d3a
shilpa.bhadra <shilpe.bhadran@gmail.com>
2023-02-23 10:27:58 +0530

    added testjava by person1

---
+++ src/test/java/sample/mavendemo/TestJava.java
@@ -0,0 +1,14 @@
+package sample.mavendemo;
+
+import org.testng.annotations.Test;
+
+public class TestJava {
+
+	
+	
+	@Test
+	public  void testjava() {
+		System.out.println("testjava added by person 1");
+
+	}
+}
